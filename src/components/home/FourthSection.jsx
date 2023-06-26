15ab454a4d3d57d096a73d541e65be51b20deab1
Mustafa <mostafakhaled0787314@gmail.com>
2023-06-26 23:02:12 +0300

    making section four home

--- src/components/home/FourthSection.jsx
+++ src/components/home/FourthSection.jsx
@@ -1,9 +1,45 @@
 import styles from "./home.module.css";
 
+import photo1 from "../../assets/home5.png";
+import photo2 from "../../assets/home6.png";
+import photo3 from "../../assets/home7.jpg";
+
 const FourthSection = () => {
   return (
     <div className={`${styles.fourth} container sections-padding`}>
-      FourthSection
+      <div className={styles.text}>
+        <p>About</p>
+        <h2>
+          Respect Your Body,
+          <br />
+          It’s the Only One You Get
+        </h2>
+        <p>
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. point of using
+          Lorem Ipsum is
+        </p>
+        <div className={styles.text_one}>
+          <div>
+            <h3>Motivation</h3>
+            <p>
+              It is a long established fact that a reader will be distracted{" "}
+            </p>
+          </div>
+          <img src={photo1} alt="Photo one" />
+        </div>
+        <div className={styles.text_two}>
+          <img src={photo2} alt="Photo two" />
+          <div>
+            <h3>Inspire</h3>
+            <p>will be distracted by readable content using Lorem Ipsum </p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.fourth_image_container}>
+        <img src={photo3} alt="Man Exercise" />
+        <button>Get Started</button>
+      </div>
     </div>
   );
 };
