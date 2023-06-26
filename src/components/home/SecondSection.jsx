a1357c161a4f7fce6c7cc820b424fc7177369e53
Mustafa <mostafakhaled0787314@gmail.com>
2023-06-26 11:58:12 +0300

    home second section

--- src/components/home/SecondSection.jsx
+++ src/components/home/SecondSection.jsx
@@ -1,9 +1,42 @@
 import styles from "./home.module.css";
+import photo1 from "../../assets/home4.png";
+import photo2 from "../../assets/home3.png";
+import photo3 from "../../assets/home2.png";
 
 const SecondSection = () => {
   return (
-    <div className={`${styles.second_section} container`}>
-      <h1>hi</h1>
+    <div className={`${styles.second_section} container sections-padding`}>
+      <div className={styles.first}>
+        <div>
+          <p>Our Fitness Training</p>
+          <h2>Upcoming Classes</h2>
+        </div>
+        <div>
+          <button>More Class</button>
+        </div>
+      </div>
+      <div className={styles.second}>
+        <div>
+          <img src={photo1} alt="women in gym" />
+          <h3>Pilates Training</h3>
+          <p>containing Lorem Ipsum passagesand more recently with </p>
+          <a href="#">Read More</a>
+        </div>
+
+        <div>
+          <img src={photo2} alt="women in gym" />
+          <h3>Aerobic Training</h3>
+          <p>containing Lorem Ipsum passagesand more recently with </p>
+          <a href="#">Read More</a>
+        </div>
+
+        <div>
+          <img src={photo3} alt="man in gym" />
+          <h3>CrossFit Workout</h3>
+          <p>containing Lorem Ipsum passagesand more recently with </p>
+          <a href="#">Read More</a>
+        </div>
+      </div>
     </div>
   );
 };
