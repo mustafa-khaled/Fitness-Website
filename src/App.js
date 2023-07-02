7a8f9d55e98fd1ffc74717955ddebde5018a6752
Mustafa <mostafakhaled0787314@gmail.com>
2023-07-02 22:39:14 +0300

    finish the home page and the footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 
 import Header from "./components/header/Header";
 import Home from "./components/home/Home.jsx";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </div>
   );
 };
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,66 @@
+import styles from "./footer.module.css";
+import logoIcon from "../../assets/Icon.png";
+
+const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <div className={styles.footer_image}>
+        <h2>
+          Follow me on
+          <a
+            href="https://www.linkedin.com/in/the-mustafa-khaled/"
+            target="_blank">
+            Linked In
+          </a>
+        </h2>
+      </div>
+      <div className={`${styles.footer_content} container `}>
+        <div>
+          <div className={styles.logo_holder}>
+            <img src={logoIcon} alt="Logo Icon" />
+
+            <h3>Strengthy</h3>
+          </div>
+          <p>
+            It is a long established fact that a reader will be distracted by
+            the readable.
+          </p>
+          <div className={styles.info}>
+            <span>Call :</span>
+            <p>01151747091</p>
+            <span>Email :</span>
+            <p>mustafakhaledinfo@gmail.com</p>
+          </div>
+        </div>
+        <div>
+          <ul>
+            <li>Utility Pages</li>
+            <li>Style Guide</li>
+            <li>Changelog</li>
+            <li>404 Page</li>
+            <li>Password Protected</li>
+            <li>Licenses</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div>
+          <h3>Newsletter</h3>
+          <input type="text" placeholder="Enter Your Email" />
+          <button>Subscribe</button>
+          <div className={styles.footer_icons}>
+            <a
+              href="https://www.linkedin.com/in/the-mustafa-khaled/"
+              target="_blank">
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+            <a href="https://github.com/mustafa-khaled" target="_blank">
+              <i className="fa-brands fa-github"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
