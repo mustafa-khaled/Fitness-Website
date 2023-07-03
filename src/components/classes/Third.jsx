57baa6aaccccaf1df80085767c8a585f6db9a29a
Mustafa <mostafakhaled0787314@gmail.com>
2023-07-03 22:27:30 +0300

    finish classes page

---
+++ src/components/classes/Third.jsx
@@ -0,0 +1,19 @@
+import styles from "./classes.module.css";
+
+const Third = () => {
+  return (
+    <div className={styles.third}>
+      <div>
+        <h2>Join Our Club</h2>
+        <p>
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. point of using
+          Lorem Ipsum is
+        </p>
+        <button>Start Now</button>
+      </div>
+    </div>
+  );
+};
+
+export default Third;
